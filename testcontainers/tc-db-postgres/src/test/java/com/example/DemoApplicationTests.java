52329b8973359b2d9772588a99d1486db9c9c957
Adib Saikali <asaikali@gmail.com>
2020-11-10 08:20:45 -0500

    Polish

--- testcontainers/tc-db-postgres/src/test/java/com/example/DemoApplicationTests.java
+++ testcontainers/tc-db-postgres/src/test/java/com/example/DemoApplicationTests.java
@@ -1,8 +1,6 @@
 package com.example;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
