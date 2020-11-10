7c438400148f88d20cbec89997a6869211245986
Adib Saikali <asaikali@gmail.com>
2020-11-10 06:38:44 -0500

    Intro demo showing how to run test containers

---
+++ testcontainers/tc-db-postgres/src/test/java/com/example/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
