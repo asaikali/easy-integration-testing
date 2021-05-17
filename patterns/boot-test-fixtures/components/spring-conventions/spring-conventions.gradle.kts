ac72e2ec77f04a6f185eb56f7daf7608865d780d
asaikali <asaikali@gmail.com>
2021-05-17 19:45:24 -0400

    Upgrade Spring Boot to 2.4.5 and to Gradle 6.8.1

--- patterns/boot-test-fixtures/components/spring-conventions/spring-conventions.gradle.kts
+++ patterns/boot-test-fixtures/components/spring-conventions/spring-conventions.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-json")
 
     testFixturesApi("org.seleniumhq.selenium:selenium-java")
-    testFixturesApi("org.testcontainers:selenium:1.15.0")
+    testFixturesApi("org.testcontainers:selenium:1.15.3")
     testFixturesApi("io.rest-assured:rest-assured")
     testFixturesApi("org.testcontainers:postgresql")
     testFixturesApi("org.testcontainers:junit-jupiter")
